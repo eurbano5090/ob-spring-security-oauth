b81ce77bd9e18c16fbecd10af26f349138da7f8a
elisanoelia <enurbanom@outlook.com>
2022-10-30 20:00:44 -0300

    cambio

---
+++ src/main/java/com/example/obspringsecurityoauth/ObSpringSecurityOauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.obspringsecurityoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObSpringSecurityOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObSpringSecurityOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/obspringsecurityoauth/ObSpringSecurityOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obspringsecurityoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObSpringSecurityOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
